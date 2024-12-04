c8982687f4f4fe4d552697bbd0e3254297bc90bc
ronnyjuny <ronny.lim@gmail.com>
2024-12-04 15:12:25 +0800

    commit

--- resources/views/layouts/partials/nav.blade.php
+++ resources/views/layouts/partials/nav.blade.php
@@ -3,7 +3,8 @@
         <div class="navbar-header">
 
             <!-- Collapsed Hamburger -->
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                data-target="#app-navbar-collapse">
                 <span class="sr-only">Toggle Navigation</span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
@@ -26,16 +27,17 @@
             <!-- Right Side Of Navbar -->
             <ul class="nav navbar-nav navbar-right">
                 <!-- Authentication Links -->
-                <?php $mark = (preg_match('/\?/', url()->current())) ? '&' : '?';?>
-                <li><a href="{{ url(url()->current() . $mark . 'lang=en') }}">en</a></li>
-                <li><a href="{{ url(url()->current() . $mark . 'lang=id') }}">id</a></li>
-                <li><a href="{{ url(url()->current() . $mark . 'lang=ur') }}">ur</a></li>
+                <?php $mark = preg_match('/\?/', url()->current()) ? '&' : '?'; ?>
+                <li><a href="{{ url(url()->current() . $mark . 'lang=en') }}">English</a></li>
+                <li><a href="{{ url(url()->current() . $mark . 'lang=id') }}">Bahasa</a></li>
+                {{-- <li><a href="{{ url(url()->current() . $mark . 'lang=ur') }}">ur</a></li> --}}
                 @if (Auth::guest())
                     <li><a href="{{ route('login') }}">Login</a></li>
                     <li><a href="{{ route('register') }}">Register</a></li>
                 @else
                     <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
+                            aria-expanded="false">
                             {{ Auth::user()->name }} <span class="caret"></span>
                         </a>
 
@@ -52,7 +54,8 @@
                                     Logout
                                 </a>
 
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                    style="display: none;">
                                     {{ csrf_field() }}
                                 </form>
                             </li>
